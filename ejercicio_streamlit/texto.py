0bb691f9289d2e7d3a1078a2909f96f7ec6b2019
martagomezz <91martagomez@gmail.com>
2022-07-22 00:16:18 +0200

    ’update’

---
+++ ejercicio_streamlit/texto.py
@@ -0,0 +1,23 @@
+texto_nox = '''
+**NOx** está formado por **NO + NO2**. Son gases que se emiten en los procesos de combustión 
+que se llevan a cabo en relación con el tráfico (sobre todo vehículos automóviles,
+y en especial de motores diésel) y con el transporte en general, así como en instalaciones 
+industriales de alta temperatura y de generación eléctrica.'''
+texto_PM25 = '''
+La materia particulada o **PM 2.5**, son partículas muy pequeñas en el aire que tiene un diámetro de 2.5 micrómetros
+ o menos de diámetro. La materia particulada, uno de los seis  criterios de contaminantes del aire de la U.S. EPA, 
+es una mezcla que puede incluir sustancias químicas orgánicas, polvo, hollín y metales. 
+Estas partículas pueden provenir de los automóviles, camiones, fábricas, quema de madera y otras actividades.
+'''
+texto_ozono = '''
+El **ozono** es un gas incoloro generalmente y de un olor acre.El ozono troposférico no se emite directamente a la atmósfera. 
+Es un contaminante secundario, esto es que se forma a partir de reacciones fotoquímicas complejas con intensa luz solar entre 
+contaminantes primarios como son los óxidos de nitrógeno (NO, NO2) 
+y compuestos orgánicos volátiles (COV) 
+'''
+texto_info = '''
+La protección del medio ambiente atmosférico implica tanto el control de las emisiones a la atmósfera como la evaluación de la calidad del aire
+a través del estudio de la concentración de contaminantes en el aire ambiente. En la ciudad de Madrid se situan diferentes estaciones 
+de control de la calidad del aire que miden la concentración de contaminantes cada hora todos los días del año. Dentro de los distintos contaminantes que se miden
+se encuentran los óxidos de nitrógeno, el material particulado y el ozono.
+'''
